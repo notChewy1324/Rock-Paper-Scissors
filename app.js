5d76d02666e6a8a150cd3950ccaf2b45a91e565e
notChewy1324 <40218645+notChewy1324@users.noreply.github.com>
2020-05-06 09:56:00 -0400

    Created Computer's Options

--- app.js
+++ app.js
@@ -15,7 +15,19 @@ const game = () => {
     }
     // Play match
     const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand');
+        const computerHand = document.querySelector('.computer-hand');
+        // Computer's options
+        const computerOptions = ['rock', 'paper', 'scissors']
 
+        options.forEach(option => {
+            option.addEventListener('click', function(){
+                // Computer choice
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoise = computerOptions[computerNumber];
+            });
+        });
     }
 
     // Calls all inner functions
